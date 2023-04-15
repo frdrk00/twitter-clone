ce0a2b6ab724a2e37d273c51e8a90bef0bf9b1e7
frdrk00 <masterkooo@gmail.com>
2023-04-15 20:06:26 +0200

    create follow and unfollow

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -6,6 +6,7 @@ import { useMemo } from 'react'
 import Button from '../Button'
 import { BiCalendar } from 'react-icons/bi'
 import useEditModal from '@/hooks/useEditModal'
+import useFollow from '@/hooks/useFollow'
 
 interface UserBioProps {
     userId: string
@@ -18,6 +19,8 @@ const UserBio: React.FC<UserBioProps> = ({
     const { data: fetchedUser } = useUser(userId)
 
     const editModal = useEditModal()
+
+    const { isFollowing, toggleFollow } = useFollow(userId)
 
     const createdAt = useMemo(() => {
         if (!fetchedUser?.createdAt) {
@@ -32,9 +35,10 @@ const UserBio: React.FC<UserBioProps> = ({
             {currentUser?.id === userId ? (
                 <Button secondary label='Edit' onClick={editModal.onOpen} />
             ): (
-                <Button onClick={() => {}}
-                label='Follow'
-                secondary
+                <Button onClick={toggleFollow}
+                label={isFollowing ? 'Unfollow' : 'Follow'}
+                secondary={!isFollowing}
+                outline={isFollowing}
                 />
             )}
         </div>
